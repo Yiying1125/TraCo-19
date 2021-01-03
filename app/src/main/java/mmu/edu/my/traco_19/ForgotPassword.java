716df1dd61f3e84e303d38d6a36381ae2ee38ffa
Yiying1125 <75288823+Yiying1125@users.noreply.github.com>
2021-01-03 17:12:56 +0800

    add back to exit for main activity and add go back button for forgot password

--- app/src/main/java/mmu/edu/my/traco_19/ForgotPassword.java
+++ app/src/main/java/mmu/edu/my/traco_19/ForgotPassword.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,6 +26,10 @@ public class ForgotPassword extends AppCompatActivity {
         setContentView(R.layout.activity_forgot_password);
         firebaseAuth=FirebaseAuth.getInstance();
         email=findViewById(R.id.editTextTextEmail);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        getSupportActionBar().setTitle("TraCo-19 (Forgot Password)");
         Button reset=findViewById(R.id.btnReset);
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,5 +52,11 @@ public class ForgotPassword extends AppCompatActivity {
                 }
             }
         });
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/mmu/edu/my/traco_19/MainActivity.java
+++ app/src/main/java/mmu/edu/my/traco_19/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 
 
+import android.os.Handler;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,11 +22,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean doubleBackToExitPressedOnce = false;
+    private Toast backPressToast;
     Version[] versions = {
             new Version("Location Service", "Track my location", R.drawable.tracklocationicon),
             new Version("Latest Update", "The latest news about Covid-19", R.drawable.updatenewsicon),
             new Version("Location History", "My location history", R.drawable.historyicon)
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,15 +41,16 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(myAdapter);
 
     }
+
     class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         ArrayList<Version> elements = new ArrayList<Version>();
+
         @NonNull
         @Override
         public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             View rowView = getLayoutInflater().inflate(R.layout.row, parent, false);
             return new MyViewHolder(rowView);
         }
-
 
 
         @Override
@@ -59,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
         public int getItemCount() {
             return elements.size();
         }
+
         public void addElements(Version[] versions) {
             elements.clear();
             elements.addAll(Arrays.asList(versions));
@@ -69,22 +76,42 @@ public class MainActivity extends AppCompatActivity {
             public TextView textView;
             public TextView textView2;
             public ImageView imageView;
+
             public MyViewHolder(@NonNull View itemView) {
                 super(itemView);
                 textView = itemView.findViewById(R.id.track);
                 textView2 = itemView.findViewById(R.id.trackldesc);
                 imageView = itemView.findViewById(R.id.trackicon);
-                itemView.setOnClickListener(this);
+                itemView.setOnClickListener(this::onClick);
             }
 
             @Override
             public void onClick(View v) {
-                String name= elements.get(getAdapterPosition()).getName();
-                startActivity(new Intent(MainActivity.this,LatestUpdateActivity.class));
+                String name = elements.get(getAdapterPosition()).getName();
+                startActivity(new Intent(MainActivity.this, LatestUpdateActivity.class));
                 finish();
             }
 
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            backPressToast.cancel();
+            super.onBackPressed();
+            return;
+        }
+        doubleBackToExitPressedOnce = true;
+        backPressToast = Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT);
+        backPressToast.show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
     }
 }
 
